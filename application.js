1a7876575b6ffd4be22260937075c3df3ccefed8
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2018-11-01 11:11:52 -0400

    Quinte Mall 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -204,7 +204,7 @@ function renderEventDetails(container, template, collection, mall_name){
 function renderFeatureItems(){
     // var items = getFeatureList();
     var items = [];
-    var images= ["//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540389339000/ACT_470X470.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540389709000/JoinTheList_470x940.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540404767000/SHOPS_470x470.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540997267000/HomePage-GiftCard-470x470_Update.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540832480000/OurStyle_470x470.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540836234000/Contact Us_Girl_600x600.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540302315000/PROMOTIONS_470x940.jpg"]
+    var images= ["//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540389339000/ACT_470X470.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540389709000/JoinTheList_470x940.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540404767000/SHOPS_470x470.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540997267000/HomePage-GiftCard-470x470_Update.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540832480000/OurStyle_470x470.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540997267000/Contact-Us_Girl_600x600_Update.jpg","//codecloud.cdn.speedyrails.net/sites/5bc90d136e6f6423ca620000/image/jpeg/1540302315000/PROMOTIONS_470x940.jpg"]
     
     var urls = ["/pages/quinte-thinking-green", "/newsletter", "/stores", "/pages/quinte-gift-cards", "/pages/quinte-guest-services", "/events", "/promotions"]
     var names = ["ACT GREEN", "NEWSLETTER", "SHOPS", "GIFT CARD", "GUEST SERVICES", "EVENTS", "PROMOTIONS"];
@@ -243,7 +243,7 @@ function renderHomeHours(container, template, collection){
     Mustache.parse(template_html);   // optional, speeds up future uses
     item_list.push(collection);    
     $.each( item_list , function( key, val ) {
-        var day = moment(val.day_of_week).format("ddd, hA"); // getDay();
+        var day = moment(val.day_of_week).format("ddd"); // getDay();
         val.day = day;
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = moment(val.open_time).tz(getPropertyTimeZone());
