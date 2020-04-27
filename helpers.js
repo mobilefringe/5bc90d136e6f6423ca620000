b1cf74263d7a9a089d1a4b18377481d118e4bae8
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-04-27 10:53:02 -0400

    Quinte Mall 2.0 1.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -261,7 +261,7 @@ function floorList() {
     var floor_1 = {};
     floor_1.id = "first-floor";
     floor_1.title = "Level One";
-    floor_1.map =  "https://www.assets.mallmaverick.com/system/site_images/photos/000/045/103/original/Quinte_Mall_-_Map_-_Oct-29-2018.svg?1540915891".split("?")[0];
+    floor_1.map =  "//assets.mallmaverick.com/system/site_images/photos/000/045/103/original/Quinte_Mall_-_Map_-_Oct-29-2018.svg?1540915891".split("?")[0];
     // getSVGMapURL().split("?")[0];
     floor_1.z_index = 1;
     floor_1.show = true;
